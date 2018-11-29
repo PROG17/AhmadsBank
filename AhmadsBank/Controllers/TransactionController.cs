bfb0f62dbc5946e1e5525803825a9199d90b91a6
Ahmad <Ahmad.a.ali@hotmail.com>
2018-11-29 16:50:02 +0100

    Test CI

--- AhmadsBank/Controllers/TransactionController.cs
+++ AhmadsBank/Controllers/TransactionController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AhmadsBank.Models;
+﻿using AhmadsBank.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AhmadsBank.Controllers
